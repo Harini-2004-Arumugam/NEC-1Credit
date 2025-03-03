a3db33e92bdcb8d17013e68cb31ce5d72cdf4ef2
Harini-2004-Arumugam <amharini2004@gmail.com>
2025-03-03 23:37:43 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,24 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+# Create an instance of the Flask class
+from flask import Flask
+app = Flask(__name__)
+
+# Define the routes and their corresponding functions
+@app.route('/')
+def personal_details():
+    return 'Hello, World!'
+
+@app.route('/name')
+def get_name():
+    return 'Harini'
+
+@app.route('/regno')
+def get_regno():
+    return '22IT012'
+
+@app.route('/department')
+def get_dept():
+    return 'Information Technology'
+
+# Run the application
+if __name__ == '_main_':
+    app.run(debug=True)
